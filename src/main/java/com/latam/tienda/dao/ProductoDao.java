b8b12706cf92956d4cd6a60d4946a0767b4b5e3b
devpfan <128647947+devpfan@users.noreply.github.com>
2023-08-21 18:31:06 -0500

    Consultas
    consultas FROM y FROM WHERE

--- src/main/java/com/latam/tienda/dao/ProductoDao.java
+++ src/main/java/com/latam/tienda/dao/ProductoDao.java
@@ -30,5 +30,9 @@ public class ProductoDao {
 		return em.createQuery(jpql,Producto.class).getResultList();
 	}
 	
+	public List<Producto> consultaPorNombre(String nombre){
+		String jpql ="SELECT P FROM Producto AS P WHERE P.nombre=:nombre";
+		return em.createQuery(jpql).setParameter("nombre",nombre).getResultList();
+	}
 
 }
